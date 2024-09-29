e3d0d35b4c598dd80d35fb65a623fd6420f4f752
Luis Lopes <lopez.assis_@outlook.com>
2024-09-29 14:19:53 -0300

    closes #4 - implementando as 4 operações

--- javascript.js
+++ javascript.js
@@ -1,7 +1,15 @@
-function calc(){
-    num1 = parseFloat(document.getElementById("num1").value);
-    num2 = parseFloat(document.getElementById("num2").value);
-    output = num1 + num2;
+function calc(op){
+    document.getElementById("sum")
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var output = 0;
 
+    switch(op){
+        case '+': output = num1 + num2; break;
+        case '-': output = num1 - num2; break;
+        case '*': output = num1 * num2; break;
+        case '/': output = num1 / num2; break;
+    }
+    
     document.getElementById("output").value = output;
 }
